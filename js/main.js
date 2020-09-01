055602e796aa5c5811b9c0609f3aea827f2c8a13
”xdan111 <1406625190@qq.com>
2020-09-01 11:32:45 +0800

    feature-0.0.2

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$("#start").css({"background":"linear-gradient(45deg,rgba(160,32,240,0.8),rgba(72,61,139,0.5),rgba(176,196,222,0.5))","height":"100hv","width":"100%","display":"flex","alignItems":"center","justifyContent":"center","position":"relative"});
+
+    /*start=document.getElementById("start");
+    start.style.background="linear-gradient(45deg,rgba(160,32,240,0.8),rgba(72,61,139,0.5),rgba(176,196,222,0.5))";
+    start.style.height="100hv";)
+    start.style.width="100%";
+    start.style.display="flex";
+    start.style.alignItems="center";
+    start.style.justifyContent="center";
+    start.style.position="relative";*/
+$("#start_one").css({"display":"grid","gridTemplateRows":"repeat(2,auto)","position":"relative"});
+
+    /*start_one=document.getElementById("start_one");
+    start_one.style.display="grid";
+    start_one.style.gridTemplateRows="repeat(2,auto)";
+    start_one.style.position="relative";*/
+
+$("#title").css({"textAlign":"center","fontsize":"48px","fontWeight":"bold","color":"gainsboro"});
+
+    /* title=document.getElementById("title");
+    title.style.textAlign="center";
+    title.style.fontSize="48px";
+    title.style.fontWeight="bold";
+    title.style.color="gainsboro";*/
+
+$("#icon").css({"position":"relative","height":"50px","color":"gainsboro"});
+
+    /*icon=document.getElementById("icon");
+    icon.style.position="relative";
+    icon.style.height="50px";
+    icon.style.width="50px";
+    icon.style.color="gainsboro";*/
+
+$("#developer").css({"fontsize":"26px","color":"gainsboro","textAlign":"center","marginTop":"15px","fontFamily":"Arial Black"});
+
+   /* developer=document.getElementById("developer");
+    developer.style.fontsize="26px";
+    developer.style.color="gainsboro";
+    developer.style.textAlign="center";
+    developer.style.marginTop="15px";
+    developer.style.fontFamily="Arial Black";*/
+
+$("#start_two").css("backgroundColor","#9091fa");
+
+    /*start_two=document.getElementById("start_two");
+    start_two.style.backgroundColor="#9091fa";*/
+
+$("#content").css({"display":"grid","gridTemplateRows":"repeat(50,50px)","gridTemplateColumns":"repeat(8,1fr)","maxWidth":"1400px","padding":"15px 0 15px 0","margin":"auto","position":"relative","gap":"0"});
+
+    /*content=document.getElementById("content");
+    content.style.display="grid";
+    content.style.gridTemplateRows="repeat(50,50px)";
+    content.style.gridTemplateColumns="repeat(8,1fr)";
+    content.style.maxWidth="1400px";
+    content.style.padding="15px 0 15px 0";
+    content.style.margin="auto";
+    content.style.position="relative";
+    content.style.gap="0";*/
+
+$("#sprite-img").css({"macWidth":"100%","maxHeight":"100%","height":"100%"});
+
+
+
