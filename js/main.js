7d2232fb023e2ad9c53092c34c0fda884feda69b
xdan111 <1406625190@qq.com>
2020-09-15 08:47:10 +0800

    feature-0.0.3

--- js/main.js
+++ js/main.js
@@ -1,63 +1,85 @@
-$("#start").css({"background":"linear-gradient(45deg,rgba(160,32,240,0.8),rgba(72,61,139,0.5),rgba(176,196,222,0.5))","height":"100hv","width":"100%","display":"flex","alignItems":"center","justifyContent":"center","position":"relative"});
+// JavaScript Document
+var divfather=document.getElementById('root');
+//顶层div
+var divtop=document.createElement('div');
+divtop.setAttribute("class","top");
 
-    /*start=document.getElementById("start");
-    start.style.background="linear-gradient(45deg,rgba(160,32,240,0.8),rgba(72,61,139,0.5),rgba(176,196,222,0.5))";
-    start.style.height="100hv";)
-    start.style.width="100%";
-    start.style.display="flex";
-    start.style.alignItems="center";
-    start.style.justifyContent="center";
-    start.style.position="relative";*/
-$("#start_one").css({"display":"grid","gridTemplateRows":"repeat(2,auto)","position":"relative"});
+//表格div
+var divcenter=document.createElement('div');
+divcenter.setAttribute("class","center");
+//房屋div
+var divbotton=document.createElement('div');
+divbotton.setAttribute("class","botton");
+divfather.appendChild(divtop);
+divfather.appendChild(divcenter);
+divfather.appendChild(divbotton);
 
-    /*start_one=document.getElementById("start_one");
-    start_one.style.display="grid";
-    start_one.style.gridTemplateRows="repeat(2,auto)";
-    start_one.style.position="relative";*/
+//画网格
+var canvas=document.createElement('canvas');
+divcenter.appendChild(canvas);
+//获取绘图环境
+var context=canvas.getContext('2d');
 
-$("#title").css({"textAlign":"center","fontsize":"48px","fontWeight":"bold","color":"gainsboro"});
-
-    /* title=document.getElementById("title");
-    title.style.textAlign="center";
-    title.style.fontSize="48px";
-    title.style.fontWeight="bold";
-    title.style.color="gainsboro";*/
-
-$("#icon").css({"position":"relative","height":"50px","color":"gainsboro"});
-
-    /*icon=document.getElementById("icon");
-    icon.style.position="relative";
-    icon.style.height="50px";
-    icon.style.width="50px";
-    icon.style.color="gainsboro";*/
-
-$("#developer").css({"fontsize":"26px","color":"gainsboro","textAlign":"center","marginTop":"15px","fontFamily":"Arial Black"});
-
-   /* developer=document.getElementById("developer");
-    developer.style.fontsize="26px";
-    developer.style.color="gainsboro";
-    developer.style.textAlign="center";
-    developer.style.marginTop="15px";
-    developer.style.fontFamily="Arial Black";*/
-
-$("#start_two").css("backgroundColor","#9091fa");
-
-    /*start_two=document.getElementById("start_two");
-    start_two.style.backgroundColor="#9091fa";*/
-
-$("#content").css({"display":"grid","gridTemplateRows":"repeat(50,50px)","gridTemplateColumns":"repeat(8,1fr)","maxWidth":"1400px","padding":"15px 0 15px 0","margin":"auto","position":"relative","gap":"0"});
-
-    /*content=document.getElementById("content");
-    content.style.display="grid";
-    content.style.gridTemplateRows="repeat(50,50px)";
-    content.style.gridTemplateColumns="repeat(8,1fr)";
-    content.style.maxWidth="1400px";
-    content.style.padding="15px 0 15px 0";
-    content.style.margin="auto";
-    content.style.position="relative";
-    content.style.gap="0";*/
-
-$("#sprite-img").css({"macWidth":"100%","maxHeight":"100%","height":"100%"});
+canvas.width='500';
+canvas.height='800';
+canvas.setAttribute("class","pen");
+//context.lineWidth=5;
+//context.strokeRect(0,0,300,200);
 
 
+
+function drawGrid(stepX, stepY, color, lineWidth){
+    // 创建垂直格网线路径
+    for(var i = 0.5 + stepX; i < canvas.width; i += stepX){
+        context.moveTo(i, 0);
+        context.lineTo(i, canvas.height);
+    }
+    // 创建水平格网线路径
+    for(var j = 0.5 + stepY; j < canvas.height; j += stepY){
+        context.moveTo(0, j);
+        context.lineTo(canvas.width, j);
+    }
+
+    // 设置绘制颜色
+    context.strokeStyle = color;
+    // 设置绘制线段的宽度
+    context.lineWidth = lineWidth;
+    // 绘制格网
+    context.stroke();
+    // 清除路径
+    context.beginPath();
+}
+drawGrid(10, 10, 'lightgray', 0.5);
+function drawAxes(origin, x_Len, y_Len, color, lineWidth){
+
+    // 创建水平坐标轴路径
+    context.moveTo(origin[0], origin[1]);
+    context.lineTo(origin[0] + x_Len, origin[1]);
+
+    // 创建垂直坐标轴路径
+    context.moveTo(origin[0], origin[1]);
+    context.lineTo(origin[0], origin[1] - y_Len);
+
+    // 创建坐标轴的刻度线路径
+    for(var i = origin[0] + 30; i < x_Len; i += 30){
+        context.moveTo(i, origin[1] - 10);
+        context.lineTo(i, origin[1] + 10);
+    }
+    for(var j = origin[1] - 30; j > origin[1] - y_Len; j -= 30){
+        // console.log(j);
+        context.moveTo(origin[0] - 10, j);
+        context.lineTo(origin[0] + 10, j);
+    }
+
+
+    // 设置绘制颜色
+    context.strokeStyle = color;
+    // 设置绘制线段的宽度
+    context.lineWidth = lineWidth;
+    // 绘制坐标轴
+    context.stroke();
+    // 清除路径
+    context.beginPath();
+}
+drawAxes([0, 790], 1920, 800, 'blue', 2);
 
