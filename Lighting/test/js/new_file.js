344f53c1e023f8a6f24d475132ffa8621c8e09ee
xdan111 <1406625190@qq.com>
2021-02-09 09:50:53 +0800

    v-1

--- Lighting/test/js/new_file.js
+++ Lighting/test/js/new_file.js
@@ -27,19 +27,30 @@ var onScroll = function(){
 	console.pageYOffset;
 	var realHight=h*5;
 	var area="Atomosphere";
+	console.log(realHight);
+
 	if (realHight <= 12000){
 		area="Troposphere 0 - 12 km";
-	}
+		}
 	else if (realHight>12000 && realHight<=45000){
 		area="Stratosphere 12 km - 45 km";
-	}
+		}
 	else if (realHight>45000 && realHight<=80000){
 		area="Mesosphere 45 km - 80 km";
-	}
+		}
 	else {
 		area="Ionosphere > 80 km";
+		}
+		zhi.innerHTML=(h*5)+" METERS HIGH"+" --- "+area;
+		zhi.style.borderTop="1px dashed";
+	if(realHight < 0){
+		zhi.innerHTML='';
+		zhi.style.borderTop="0px";
 	}
-	zhi.innerHTML=(h*5)+" METERS HIGH"+" --- "+area;
+		
+
+	
+			
 }
 
 //给页面绑定滑轮滚动事件,滑轮滚动时触发scrollFunc方法
@@ -48,5 +59,4 @@ document.addEventListener('DOMMouseScroll', scrollFunc, false);
 } */
 //页面滚动触发
 window.onscroll = onScroll;
-		   
 		   
